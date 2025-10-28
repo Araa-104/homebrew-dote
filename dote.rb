babb37b0f3e809f94cf72e10040ccec2f91c1d70
Araa-104 <a_yuna.1203@icloud.com>
2025-10-28 11:41:54 +0900

    version “1.0.1”

--- dote.rb
+++ dote.rb
@@ -2,8 +2,8 @@ class Dote < Formula
   desc "dot e command"
   homepage "https://github.com/Araa-104/homebrew-dote"
   url "https://raw.githubusercontent.com/Araa-104/homebrew-dote/main/dote.c"
-  sha256 "87fc2c2b530884176a1a51b99af09db6052d806a15d635bc19c94f7cb7dc5250"
-  version "1.0.0"
+  sha256 "6be7defd3eca4430ae2647963fd13544e894b885200b74bb45cdbc62ed32d207"
+  version "1.0.1"
 
   def install
     system ENV.cc, "dote.c", "-o", "dote"
