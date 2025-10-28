8a55001527251fb69ef089286f36551ab043add4
Araa-104 <a_yuna.1203@icloud.com>
2025-10-28 12:11:50 +0900

    Update dote.rb

--- dote.rb
+++ dote.rb
@@ -3,7 +3,7 @@ class Dote < Formula
   homepage "https://github.com/Araa-104/homebrew-dote"
   url "https://raw.githubusercontent.com/Araa-104/homebrew-dote/main/dote.c"
   sha256 "829273d00f8aad98f3295374efe542871086b24719cada99d011916238178d55"
-  version "1.0.2"
+  version "1.0.3"
 
   def install
     system ENV.cc, "dote.c", "-o", "dote"
